af09796942457cffcdc02fb2edbf5af8cde4f7e5
Shivam-Chaubey <shivamspit@gmail.com>
2025-02-28 22:44:19 +0530

    ReverseInt with only 32 bit values

--- src/basic/ReverseNumber.java
+++ src/basic/ReverseNumber.java
@@ -6,7 +6,9 @@ public class ReverseNumber {
     public static void main(String[] args) {
         Random random = new Random();
         long n = random.nextLong(999999999);
+        int intN = random.nextInt(999999999);
         System.out.println(n + " : " + reverseNumber(n));
+        System.out.println(intN + " : " + reverse(intN));
     }
 
     private static long reverseNumber(long n) {
@@ -16,6 +18,19 @@ public class ReverseNumber {
             n = n / 10;
         }
         return rev;
+    }
+
+    private static int reverse(int x) {
+        int sign = x < 0 ? -1 : 1, positiveNum = Math.abs(x), reverseNum = 0, digit;
+        while(positiveNum != 0){
+            digit = positiveNum % 10;
+            if (reverseNum > (Integer.MAX_VALUE - digit) / 10) {
+                return 0;
+            }
+            reverseNum = reverseNum * 10 + digit;
+            positiveNum /= 10;
+        }
+        return reverseNum*sign;
     }
 }
 
