7e32ff4e92571ec0458da7090d6af6eb996fe898
Shivam-Chaubey <shivamspit@gmail.com>
2025-03-09 16:54:01 +0530

    print value using recursion

---
+++ src/recursion/PrintValue.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class PrintValue {
+    public static void main(String[] args) {
+        printGfg(10);
+    }
+
+    static int val = 1;
+    private static void printGfg(int N) {
+        // code here
+        while(val <= N){
+            System.out.print("GFG" + " ");
+            val++;
+            printGfg(N);
+        }
+    }
+}
