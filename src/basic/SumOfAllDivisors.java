7577e77b3409a57999433860acc79beec4b36f4c
Shivam-Chaubey <shivamspit@gmail.com>
2025-03-09 15:10:59 +0530

    sumOfAllDivisors

--- src/basic/SumOfAllDivisors.java
+++ src/basic/SumOfAllDivisors.java
@@ -2,7 +2,12 @@ package basic;
 
 public class SumOfAllDivisors {
     public static void main(String[] args) {
-        System.out.println(sumOfAllDivisors(10));
+        int n = 10;
+        System.out.println(sumOfAllDivisors(n));
+        System.out.println(sumOfAllDivisorsApproach2(n));
+        System.out.println(sumOfAllDivisorsApproach3(n));
+        System.out.println(sumOfAllDivisorsSieve(n));
+        System.out.println(sumOfAllDivisorsOptimizedHarmonicLemma(n));
     }
     private static int sumOfAllDivisors(int n){
         // Write your code here.
@@ -16,4 +21,58 @@ public class SumOfAllDivisors {
         }
         return sum;
     }
+
+    private static int sumOfAllDivisorsApproach2(int n){
+        // Write your code here.
+        int sum = 0;
+
+        for (int i = 1; i <=n; i++) {
+            sum = sum+(n/i)*i;
+        }
+        return sum;
+    }
+
+    public static int sumOfAllDivisorsApproach3(int n){
+        int ans = 0;
+        int i = 1;
+        while (i <= n){
+            int val = n / i;
+            ans = ans + (i * val);
+            i = i + 1;
+        }
+        return ans;
+    }
+
+    public static int sumOfAllDivisorsSieve(int n) {
+        int[] sumDivisors = new int[n + 1];
+
+        // Iterate through all numbers
+        for (int i = 1; i <= n; i++) {
+            // Add i to all its multiples
+            for (int j = i; j <= n; j += i) {
+                sumDivisors[j] += i;
+            }
+        }
+
+        // Compute the sum of all divisor sums
+        int totalSum = 0;
+        for (int i = 1; i <= n; i++) {
+            totalSum += sumDivisors[i];
+        }
+
+        return totalSum;
+    }
+
+    public static int sumOfAllDivisorsOptimizedHarmonicLemma(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += (n / i) * i;  // Directly add contributions
+        }
+
+        return sum;
+    }
+
+
+
 }
