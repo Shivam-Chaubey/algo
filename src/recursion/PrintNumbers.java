004b96ade4ce4f6860ccd28247ac5706169886f6
Shivam-Chaubey <shivamspit@gmail.com>
2025-03-09 17:05:12 +0530

    reverse nos using recusrsion

--- src/recursion/PrintNumbers.java
+++ src/recursion/PrintNumbers.java
@@ -2,7 +2,11 @@ package recursion;
 
 public class PrintNumbers {
     public static void main(String[] args) {
+        System.out.print("Ascending ");
         printNos(10);
+        System.out.println();
+        System.out.print("Descending ");
+        printReverseNos(10);
     }
 
     static int val = 1;
@@ -13,5 +17,15 @@ public class PrintNumbers {
         System.out.print(val + " ");
         val++;
         printNos(n);
+    }
+
+    static int val2 = 1;
+    private static void printReverseNos(int N) {
+        // code here
+        if(N < val2)
+            return;
+        System.out.print(N + " ");
+        N--;
+        printReverseNos(N);
     }
 }
