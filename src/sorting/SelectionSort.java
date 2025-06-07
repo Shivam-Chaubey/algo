3c4f5ab92c26156deaad2a0d9255708fdec9fca1
Shivam-Chaubey <shivamspit@gmail.com>
2025-06-07 23:52:32 +0530

    Striver selection sort

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -5,10 +5,13 @@ import common.CommonUtility;
 public class SelectionSort extends CommonUtility {
     public static void main(String[] args) {
         int[] arr = generateRandomArray(10);
+        printArray(arr);
         int[] resultIDE = selectionSortIDE(arr);
         int[] resultSelf = selectionSortSelf(arr);
+        int[] sortedStriver = selectionSortStriver(arr);
         printArray(resultIDE);
         printArray(resultSelf);
+        printArray(sortedStriver);
     }
 
     public static int[] selectionSortSelf(int[] arr) {
@@ -42,6 +45,20 @@ public class SelectionSort extends CommonUtility {
         return arr;
     }
 
+    private static int[] selectionSortStriver(int[] unsortedArray){
+        int n = unsortedArray.length;
+        int mini_index;
+        for(int i = 0; i < n - 2; i++){
+            mini_index = i;
+            for(int j = i+1 ; j < n; j++){
+                if(unsortedArray[j] < unsortedArray[mini_index]){
+                    mini_index = j;
+                }
+            }
+            swap(unsortedArray, mini_index, i);
+        }
+        return unsortedArray;
+    }
 }
 
 
