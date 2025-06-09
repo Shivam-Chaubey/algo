3e1f0673930590737e02cc8b0a1076b9ae396fe9
Shivam-Chaubey <shivamspit@gmail.com>
2025-06-10 01:27:42 +0530

    making for loop instead of while

--- src/sorting/InsertionSortRecurive.java
+++ src/sorting/InsertionSortRecurive.java
@@ -12,11 +12,13 @@ public class InsertionSortRecurive extends CommonUtility{
     private static void insertionSort(int[] arr, int i, int n){
         if(i==n)
             return;
-        int j = i;
-        while(j>0 && arr[j-1] > arr[j]){
+        for(int j=i; j>0 && arr[j]<arr[j-1]; j--){
             swap(arr, j, j-1);
-            j--;
         }
+        // int j = i;
+        // while(j>0 && arr[j]<arr[j-1]){
+        //     swap(arr, j, j-1);
+        // }
         insertionSort(arr, i+1, n);
     }
 }
